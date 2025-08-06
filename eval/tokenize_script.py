8dab30405ac93fd934237f0be3435509b61e111f
zhangliangyu32 <zhangliangyu32@users.noreply.github.com>
2025-08-06 11:15:13 +0800

    Implement the training loop and the experiment script.

--- eval/tokenize_script.py
+++ eval/tokenize_script.py
@@ -5,13 +5,13 @@ tokenizer_owt = Tokenizer.from_files(
     merges_filepath="owt_merges.pkl",
     special_tokens=["<|endoftext|>"]
 )
-tokenizer_owt.encode_to_file(load_path="../data/owt_train.txt", save_path="../data/owt_train.npy")
+tokenizer_owt.encode_to_file(load_path="/root/autodl-tmp/data/owt_train.txt", save_path="/root/autodl-tmp/data/owt_train.npy")
 
-tokenizer_tiny_stories = Tokenizer.from_files(
-    vocab_filepath="tiny_stories_vocabulary.pkl",
-    merges_filepath="tiny_stories_merges.pkl",
-    special_tokens=["<|endoftext|>"]
-)
-tokenizer_tiny_stories.encode_to_file(
-    load_path="../data/TinyStoriesV2-GPT4-train.txt",
-    save_path="../data/tiny_stories_train.npy")
+# tokenizer_tiny_stories = Tokenizer.from_files(
+#     vocab_filepath="tiny_stories_vocabulary.pkl",
+#     merges_filepath="tiny_stories_merges.pkl",
+#     special_tokens=["<|endoftext|>"]
+# )
+# tokenizer_tiny_stories.encode_to_file(
+#     load_path="/root/autodl-tmp/data/TinyStoriesV2-GPT4-train.txt",
+#     save_path="/root/autodl-tmp/data/tiny_stories_train.npy")
---
+++ eval/training_script.py
@@ -0,0 +1,49 @@
+
+config = {
+        "batch_size": 32,
+        "context_len": 256,
+        "max_iterations": 40000,
+        "weight_decay": 0.00,
+        "save_until": 100000,
+        "save_path": "/root/assignment1-basics/checkpoints/",
+        "val_until": 2000,
+        "learning_rate": 3e-3,
+        "lr_scheduler_cosine": False,
+        "max_learning_rate": 1e-3,
+        "min_learning_rate": 1e-6,
+        "warmup_end": 4000,
+        "cosine_end": 40000,
+        "device": "cuda:0",
+        "train_data_path": "/root/autodl-tmp/data/tiny_stories_train.npy",
+        "val_data_path": "/root/autodl-tmp/data/tiny_stories_valid.npy",
+    }
+from cs336_basics.Modules import TransformerLM
+import numpy as np
+from cs336_basics.train import train
+x_train = np.memmap(config["train_data_path"], dtype=np. uint16, mode='r').astype(np.int64)
+x_val = np.memmap(config["val_data_path"], dtype=np.uint16, mode='r').astype(np.int64)
+
+config["learning_rate"] = 1e-4
+model = TransformerLM(vocab_size=10000, context_length=config["context_len"], num_layers=4, d_model=512, num_heads=16, d_ff=1344, rope_theta=10000, device=config["device"])
+train(x_train, x_val, model, config)
+
+config["learning_rate"] = 2e-3
+config["batch_size"] = 64
+config["max_iterations"] = 20000
+model = TransformerLM(vocab_size=10000, context_length=config["context_len"], num_layers=4, d_model=512, num_heads=16, d_ff=1344, rope_theta=10000, device=config["device"])
+train(x_train, x_val, model, config)
+
+config["learning_rate"] = 5e-4
+config["batch_size"] = 16
+config["max_iterations"] = 80000
+model = TransformerLM(vocab_size=10000, context_length=config["context_len"], num_layers=4, d_model=512, num_heads=16, d_ff=1344, rope_theta=10000, device=config["device"])
+train(x_train, x_val, model, config)
+
+config["max_iterations"] = 40000
+config["batch_size"] = 32
+config["learning_rate"] = 1e-3
+config["weight_decay"] = 0.01
+config["lr_scheduler_cosine"] = True
+config["note"] = "cosine learning rate scheduler and weight decay"
+model = TransformerLM(vocab_size=10000, context_length=config["context_len"], num_layers=4, d_model=512, num_heads=16, d_ff=1344, rope_theta=10000, device=config["device"])
+train(x_train, x_val, model, config)
